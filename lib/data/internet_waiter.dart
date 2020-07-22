46f155dd7241953646560a67508972bae511f632
Chris Rutkowski <chris.rutkowski@agoda.com>
2020-07-22 07:51:18 +0700

    no message

--- lib/data/internet_waiter.dart
+++ lib/data/internet_waiter.dart
@@ -1,15 +1,18 @@
 import 'package:data_connection_checker/data_connection_checker.dart';
 
 abstract class InternetWaiter {
+  Future<bool> get online;
   Future wait();
 }
 
 class InternetWaiterImpl implements InternetWaiter {
   @override
+  Future<bool> get online => DataConnectionChecker().hasConnection;
+
+  @override
   Future wait() async {
-    if (await DataConnectionChecker().hasConnection) {
-      return true;
-    }
+    if (await online) return true;
+
     await Future.delayed(Duration(milliseconds: 1000));
     return await wait();
   }
