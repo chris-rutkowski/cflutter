9b74b3ab5ab08e72c56a77609a90a9d5a98a37d0
Chris Rutkowski <chris.rutkowski@agoda.com>
2020-08-20 09:17:57 +0700

    allows to customise the loading indicator

--- lib/presentation/widgets/loading_indicator.dart
+++ lib/presentation/widgets/loading_indicator.dart
@@ -5,15 +5,18 @@ import '../utils/theme/space.dart';
 
 class LoadingIndicator extends StatelessWidget {
   final double size;
+  static Widget Function(BuildContext context) builder;
 
   LoadingIndicator({Key key, this.size = 96}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SpinKitRing(
-      color: Theme.of(context).colorScheme.primary,
-      size: size,
-      lineWidth: xSmall * size / 96,
-    );
+    return builder != null
+        ? builder(context)
+        : SpinKitRing(
+            color: Theme.of(context).colorScheme.primary,
+            size: size,
+            lineWidth: xSmall * size / 96,
+          );
   }
 }
