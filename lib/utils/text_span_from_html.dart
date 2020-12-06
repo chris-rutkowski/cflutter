98e1c97012998a85b34b246b5a61814580b12f25
Chris Rutkowski <chrisrutkowski89@gmail.com>
2020-12-06 09:19:12 +0700

    drops few lines

--- lib/utils/text_span_from_html.dart
+++ lib/utils/text_span_from_html.dart
@@ -3,13 +3,10 @@ import 'package:flutter/material.dart';
 import 'indexed_iterable.dart';
 
 List<TextSpan> textSpanFromHTML(String html) {
-  final components = html.split(RegExp(r'<[/]?b>'));
-
-  return components
+  return html
+      .split(RegExp(r'<[/]?b>'))
       .mapIndexed(
-        (i, e) => TextSpan(
-            text: e,
-            style: i.isOdd ? TextStyle(fontWeight: FontWeight.bold) : null),
+        (i, e) => TextSpan(text: e, style: i.isOdd ? TextStyle(fontWeight: FontWeight.bold) : null),
       )
       .toList();
 }
