cd416659a79a7a07727401e60bed23ec193ddd10
Chris Rutkowski <chrisrutkowski89@gmail.com>
2020-12-17 19:29:34 +0700

    allows specify bold font

--- lib/utils/text_span_from_html.dart
+++ lib/utils/text_span_from_html.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 
 import 'indexed_iterable.dart';
 
-List<TextSpan> textSpanFromHTML(String html) {
+List<TextSpan> textSpanFromHTML(String html, {FontWeight boldFontWeight = FontWeight.bold}) {
   return html
       .split(RegExp(r'<[/]?b>'))
       .mapIndexed(
-        (i, e) => TextSpan(text: e, style: i.isOdd ? TextStyle(fontWeight: FontWeight.bold) : null),
+        (i, e) => TextSpan(text: e, style: i.isOdd ? TextStyle(fontWeight: boldFontWeight) : null),
       )
       .toList();
 }
